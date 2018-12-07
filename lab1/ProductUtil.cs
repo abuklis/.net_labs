6ba0119f011e6769fa398a27c84fef25e26b631e
Hanna <anyabukliss@gmail.com>
2018-12-07 13:04:22 +0300

    Lab 1

---
+++ lab1/ProductUtil.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace lab1
+{
+    public static class ProductUtil
+    {
+        private const string UpcPattern = "^[0-9]{12}$";
+        private static readonly Regex UpcRegex = new Regex(UpcPattern);
+
+        public static bool IsValidUpc(string upc) {
+            return !string.IsNullOrEmpty(upc) && UpcRegex.IsMatch(upc);
+        }
+        
+        public static List<Product> FindProductsByName(Product[] products, string name) {
+            var productsList = new List<Product>();
+            if (IsProductArrayValid(products) && !string.IsNullOrEmpty(name)) {
+                foreach (var product in products) {
+                    if (AreStringsEqualIgnoreCase(product.GetName(), name)) {
+                        productsList.Add(product);
+                    }
+                }
+            } else {
+                Console.Write("Invalid params");
+            }
+            return productsList;
+        }
+
+        public static List<Product> FindProductsByNameAndBelowProvidedPrice(Product[] products, string name,
+            double price) {
+            var productsList = new List<Product>();
+            
+            if (IsProductArrayValid(products) && !string.IsNullOrEmpty(name)) {
+                foreach (var product in products) {
+                    if (AreStringsEqualIgnoreCase(product.GetName(), name) && product.GetPrice() < price) {
+                        productsList.Add(product);
+                    }
+                }
+            } else {
+                Console.Write("Invalid params");
+            }
+            return productsList;
+        }
+        
+        public static List<Product> FindProductsStoredMoreThanProvidedPeriod(Product[] products, int shelfLifeMonth) {
+            var productsList = new List<Product>();
+            if (IsProductArrayValid(products)) {
+                foreach (var product in products) {
+                    if (product.GetShelfLifeMonth() > shelfLifeMonth) {
+                        productsList.Add(product);
+                    }
+                }
+            } else {
+                Console.Write("Invalid params");
+            }
+            return productsList;
+        }
+
+        private static bool AreStringsEqualIgnoreCase(string firstString, string secondString) {
+            return firstString.Equals(secondString, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static bool IsProductArrayValid(Product[] products) {
+            return products != null && products.Any();
+        }
+    }
+}
---
+++ lab1/Runner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Runner
+    {
+        public static void Main(string[] args)
+        {
+            var product1  = new Product(1, "Phone", "123456789123", "Samsung", 200, 14, 20);
+            var product2  = new Product(2, "TV", "123456789127", "Apple",300 , 17, 13);
+            var product3  = new Product(3, "Xerox", "123456789123", "HP", 400, 19, 2);
+            
+            var products = new Product[3];
+            products[0] = product1;
+            products[1] = product2;
+            products[2] = product3;
+
+
+            Console.WriteLine("--All TVs:");
+            ProductUtil.FindProductsByName(products, "TV").ForEach(Console.WriteLine);
+            
+            Console.WriteLine("--Phones with price below 300:");
+            ProductUtil.FindProductsByNameAndBelowProvidedPrice(products, "Phone", 300).ForEach(Console.WriteLine);
+            
+            
+            Console.WriteLine("--Products which lies on the shelf more than 17 months");
+            ProductUtil.FindProductsStoredMoreThanProvidedPeriod(products, 16).ForEach(Console.WriteLine);
+        }
+    }
+}
